2e787f3c64597183bb52b941e2f52267b320b1e8
Piotr Turski <piotr.turski@gmail.com>
2019-02-18 02:13:18 +0100

    rx getHeroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,15 @@
 import { HEROES } from './heroes/mock-heroes';
 import { Hero } from './heroes/hero';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-getHeroes(): Hero[] {
-  return HEROES
+getHeroes(): Observable<Hero[]> {
+  return of(HEROES)
 }
 
   constructor() { }
