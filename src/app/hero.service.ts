64e51835d2ab84c448deb132e98e59fe93ecf873
Piotr Turski <piotr.turski@gmail.com>
2019-02-20 03:06:28 +0100

    update  hero via http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,15 @@
 import { MessageService } from './message.service';
 // import { HEROES } from './heroes/mock-heroes';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './heroes/hero';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { log } from 'util';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +33,14 @@ export class HeroService {
         tap(_ => this.log(`HeroService: fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
       );
+  }
+
+  /** PUT: update the hero on the server */
+  update(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
   private log(message: string) {
